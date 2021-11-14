210b008855b6db9c75ebea75b8077ad25afd27c1
Kike <kike@gmail.com>
2021-11-13 22:23:08 -0600

    Examen Final DW - Carlos Monterroso

---
+++ dwFinal/Data/dwFinalContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using dwFinal.Models;
+
+namespace dwFinal.Data
+{
+    public class dwFinalContext : DbContext
+    {
+        public dwFinalContext (DbContextOptions<dwFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<dwFinal.Models.cursos> cursos { get; set; }
+
+        public DbSet<dwFinal.Models.catedraticos> catedraticos { get; set; }
+
+        public DbSet<dwFinal.Models.reportes> reportes { get; set; }
+    }
+}
---
+++ dwFinal/Models/reportes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dwFinal.Models
+{
+    public class reportes
+    {
+        public int id { get; set; }
+        public int catedraticos_activos { get; set; }
+        public int catedraticos_inactivos { get; set; }
+        public int catedraticos_reprobados { get; set; }
+    }
+}
